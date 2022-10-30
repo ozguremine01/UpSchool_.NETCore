f9299d0c8229c41634b9d210e153800a3c8a7bf2
Pc <Pc@DESKTOP-H7B28ES>
2022-10-30 15:27:30 +0300

    Add project files.

---
+++ CrmUpSchool.Entity/Concrete/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmUpSchool.Entity.Concrete
+{
+    class Customer
+    {
+        [Key]
+        public int CustomerID { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public string CustomerSurname { get; set; }
+
+        public string CustomerPhone { get; set; }
+
+        public string CustomerMail { get; set; }
+    }
+}
---
+++ UpSchool_.NETCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UpSchool_.NETCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
