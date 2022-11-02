51f70e1631b5ab6af5bbbc89cb28dc0015a0b6e8
ozgur.emine <ozguremine01@gmail.com>
2022-11-02 14:24:48 +0300

    UpSchool & Akbank Ders - 2 Kasım 2022 - .NET Core

---
+++ UpSchool.BusinessLayer/Concrete/EmployeeManagercs.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UpSchool.BusinessLayer.Abstract;
+using UpSchool.DataAccessLayer.Abstract;
+using UpSchool.EntityLayer.Concrete;
+
+namespace UpSchool.BusinessLayer.Concrete
+{
+    public class EmployeeManagercs:IEmployeeService
+    {
+        IEmployee _category;
+
+        public EmployeeManagercs(IEmployee category)
+        {
+            _category = category;
+        }
+
+        public void Delete(Emploee t)
+        {
+            _category.Delete(t);
+        }
+
+        public Emploee GetByID(int id)
+        {
+            return _category.GetByID(id);
+        }
+
+        public List<Emploee> GetList()
+        //public List<Category> TGetList()
+        {
+            //return _category.GetList(t);
+            return _category.GetList();
+        }
+
+        public void Insert(Emploee t)
+        {
+            //if(t.CategoryName !=null && t.CategoryName.Length >=5 && t.CategoryDescription.StartsWith("A"))
+            //{
+            //    _category.Insert(t);
+            //}
+            //else
+            //{
+
+            //}
+            _category.Insert(t);
+        }
+
+        public void Update(Emploee t)
+        {
+            _category.Update(t);
+        }
+    }
+}
---
+++ UpSchool.BusinessLayer/ValidationRules/EmployeeValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UpSchool.EntityLayer.Concrete;
+
+namespace UpSchool.BusinessLayer.ValidationRules
+{
+    public class EmployeeValidator : AbstractValidator<Emploee>
+    {
+        public EmployeeValidator()
+        {
+            RuleFor(x => x.EmployeeName).NotEmpty().WithMessage("Personel adını boş geçemezsiniz");
+            RuleFor(x => x.EmployeeSurname).NotEmpty().WithMessage("Personel soyadını boş geçemezsiniz");
+            RuleFor(x => x.EmployeeName).MinimumLength(2).WithMessage("En az 2 karakter giriniz");
+            RuleFor(x => x.EmployeeName).MaximumLength(20).WithMessage("En fazla 20 karakter giriniz");
+        }
+    }
+}
